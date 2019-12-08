0590970165082ee763a5dc58472cb194a4a3d8c9
Andriy Stefanchuk <>
2019-12-08 17:22:16 +0200

    Initial commit

---
+++ CoordinatorTest/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  CoordinatorTest
+//
+//  Created by Andriy Stefanchuk on 08.12.2019.
+//  Copyright © 2019 UCUTalk. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ CoordinatorTest/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by Andriy Stefanchuk on 08.12.2019.
+//  Copyright © 2019 UCUTalk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
